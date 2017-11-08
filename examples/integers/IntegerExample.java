5431e9415e39b23ab6630debf833d2cc1703f5cd
Lasse <lassebe@kth.se>
2017-11-08 17:38:19 +0100

    removed print from integerexample to focus the example, also added iterations to show a more dramatic performance improvement

--- examples/integers/IntegerExample.java
+++ examples/integers/IntegerExample.java
@@ -8,7 +8,6 @@ public class IntegerExample {
     int i = 100;
     int y = 500;
     while(i-- != 0) {
-      System.out.println("Simulate work " + i);
       y += 47;
       y *= 22;
       y--;
@@ -26,7 +25,7 @@ public class IntegerExample {
     IntegerExample ex = new IntegerExample();
     assert(ex.getXPlusFive() == 5);
 
-    for(int i=0;i<100; i++) {
+    for(int i=0;i<10000; i++) {
       assert(ex.getXPlusFive() == 5);
     }
     ex.incrementX();
